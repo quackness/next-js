603bc786d259868447fdf27255702556ab0cdf98
Karolina Redden <dubajkaro@gmail.com>
2024-02-24 17:22:36 -0400

    tag a componennt in the app folder to be a client resource

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+function ProductCard() {
+  return (
+    <div>
+      <button onClick={()=> console.log('Click')}>Add to card</button>
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
-<main>Hello World</main>
+<main>
+  <h1>Hello World</h1>
+  <Link href="/users">Users</Link>
+  <ProductCard />
+</main>
   )
 }
