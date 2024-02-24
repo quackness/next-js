4ac4dbe44a8de5da1035c667f97812c447bba4db
Karolina Redden <dubajkaro@gmail.com>
2024-02-24 18:17:40 -0400

    disable caching ins erver side

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,15 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    //works with fetch only
-    next: {revalidate: 10}
+      //revalidate works with fetch only
+    // next: {revalidate: 10}
+    cache: 'no-store'
   });
   const users: User[] = await res.json()
   return (
    <>
    <h1>Users:</h1>
+   <p>{new Date().toLocaleTimeString()}</p>
    <ul>
     {users.map((user) => <li key={user.id}>{user.name}</li>)}
 
