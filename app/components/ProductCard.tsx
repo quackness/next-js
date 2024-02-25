7f0e4d1160bd68b7aeab6bc5f1f47c7e7d8e428e
Karolina Redden <dubajkaro@gmail.com>
2024-02-25 11:17:55 -0400

    use tailwinf utility classes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -5,7 +5,8 @@ import styles from './ProductCard.module.css';
 
 function ProductCard() {
   return (
-    <div className={styles.card}>
+    // <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
   <AddToCart />
     </div>
   )
